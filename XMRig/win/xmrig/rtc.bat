ee85cd8a5de74087d1daa36a6eb207b3dc8c988d
FTech's <60128047+devfsoft06@users.noreply.github.com>
2023-12-12 11:48:23 +0700

    Fixed error invalid wallet

--- XMRig/win/xmrig/rtc.bat
+++ XMRig/win/xmrig/rtc.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-xmrig.exe -a ghostrider --url stratum-asia.rplant.xyz:17054 --tls --user RisZ9viLY8jsULoB9a1xxdxAZAV5LLyboj.win --randomx-1gb-pages ---daemon -t 6
+xmrig.exe -a ghostrider --url stratum-asia.rplant.xyz:17054 --tls --user RisZ9viLY8jsULoB9a1xxdxAZAV5LLyboj.hehe --randomx-1gb-pages -p -x -t 6
 pause
